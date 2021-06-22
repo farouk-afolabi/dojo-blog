4a5f9864259e34b1a4717b40f39c4e830038dcf4
farouk afolabi <afolabifarouk99@gmail.com>
2021-06-22 10:31:54 +0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 
-import './App.css';
 import Navbar from './Navbar';
+import Home from './Home'
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
- // const person = { name: 'yoshi', age: 30 };
-  const link = "http://www.google.com";
+ 
   return (
     <div className="App">
+      < Navbar />
       <div className="content">
-        <h1> { title } </h1>
-        <p>Liked { likes } times</p>
-       
-        {/* <p> { person } </p> */}
-
-        <p>{ 10 } </p>
-        <p>{ "hello, ninjas" } </p>
-        <p>{ [1,2,3,4,5] } </p>
-        <p>{ Math.random() * 20 }</p>
-
-        <a href={link}> Google site </a>
+        <Home/>
       </div>
     </div>
   );

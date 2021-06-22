8306ea3eae0db12cdb4aa707c8c85b99f6f89b23
farouk afolabi <afolabifarouk99@gmail.com>
2021-06-22 11:57:46 +0300

    click functions

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,17 @@
 const Home = () => {
-    const handleClick = () => {
-        console.log('hello, ninjas');
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
     }
+
+    const handleClickAgain = (name,e) => {
+        console.log ('hello ' + name, e.target);
+    }
+
     return (
         <div className="home"> 
         <h2>Homepage</h2>
         <button onClick={handleClick}>Click me </button>
+        <button onClick ={() => handleClickAgain('mario')}>Click me again</button>
         </div>
       );
 }

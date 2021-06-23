5620c11216a795bf5b085b7d9ae954d0df348f87
farouk afolabi <afolabifarouk99@gmail.com>
2021-06-23 13:34:40 +0300

    for changing values with react hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
 const Home = () => {
+   // let name = 'mario';
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('luigi');
+        setAge('38');
+
+    }
     return (
         <div className="home"> 
         <h2>Homepage</h2>
+        <p> {name} is {age} years old </p>
+        <button onClick= {handleClick}> Click me</button>
         </div>
       );
 }

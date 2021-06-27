c06fe326c3ab0b695b0b1c7b3516e23b774ff104
farouk afolabi <afolabifarouk99@gmail.com>
2021-06-28 00:45:48 +0300

    delete blogs

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs,title }) => {
+const BlogList = ({ blogs,title, handleDelete }) => {
   //  const blogs = props.blogs;
   //  const title = props.title;
     
@@ -11,6 +11,7 @@ const BlogList = ({ blogs,title }) => {
         <div className="blog-preview" key={blog.id} >
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
+          <button onClick={() => handleDelete(blog.id)} > delete blog </button>
         </div>
       ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -7,13 +7,20 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);  
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter( blog => blog.id !== id)
+         setBlogs(newBlogs);
+    }
+     
     return (
         <div className="home"> 
         
-        <BlogList blogs={blogs} title="All Blogs!" />
-        <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" />
+        <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+    
         </div>
       );
 }
  
 export default Home;
+ 

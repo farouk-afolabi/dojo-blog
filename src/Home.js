594e9d79fed2f4563f8ccc4d67cdaee726abece6
farouk afolabi <afolabifarouk99@gmail.com>
2021-06-29 12:42:28 +0300

    conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,25 +3,26 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null); 
-    
-    const [name, setName] = useState ('mario');
+    const [isPending, setIsPending] = useState(true);
 
     
     useEffect(() => {
+        setTimeout(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
           return  res.json();
         })
         .then(data => {
-            console.log(data);
             setBlogs(data);
+            setIsPending(false);
 
-        })
+        });
+    }, 1000);
     }, []);
      
     return (
         <div className="home"> 
-        
+       { isPending && <div> Loading ... </div> } 
        { blogs  &&  <BlogList blogs={blogs} title="All Blogs!" /> }
         
         </div>

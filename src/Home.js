1eef3ca6c23b0834b1dfedf848112b79b96946ca
farouk afolabi <afolabifarouk99@gmail.com>
2021-06-29 11:40:24 +0300

    fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -6,26 +6,24 @@ const Home = () => {
     
     const [name, setName] = useState ('mario');
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter( blog => blog.id !== id)
-         setBlogs(newBlogs);
-    }
+    
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
-            res.json();
+          return  res.json();
         })
         .then(data => {
-            
+            console.log(data);
+            setBlogs(data);
+
         })
     }, []);
      
     return (
         <div className="home"> 
         
-        <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-        <button onClick= {() => setName('luigi')}>change name </button>
-        <p>{name}</p>
+       { blogs  &&  <BlogList blogs={blogs} title="All Blogs!" /> }
+        
         </div>
       );
 }

de75e0e81b9e96816291e8bafe1a0bdf10187829
farouk afolabi <afolabifarouk99@gmail.com>
2021-07-03 08:44:44 +0300

    useEffect cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
     const [ error, setError] = useState(null);
     
     useEffect(() => {
+     const abortCont = new AbortController();
+
         setTimeout(() => {
-        fetch(url)
+        fetch(url, {signal: abortCont.signal })
         .then(res => {
             if(!res.ok) { // error coming back from the server 
                 throw Error('could not fetch the data for that resource');
@@ -21,11 +23,17 @@ const useFetch = (url) => {
 
         })
         .catch(err => { // auto catches network / connection error
-            setIsPending(false);
-            setError(err.message);
-        })
+            if (err.name === 'AbortError'){
+                console.log ('fetch aborted');
+            } else {
+                setIsPending(false);
+                setError(err.message);
+            }
+            })
+           
     }, 1000);
-    }, [url])
+    return () =>  abortCont.abort();
+    }, [url]);
     
     return {data, isPending, error};
 }

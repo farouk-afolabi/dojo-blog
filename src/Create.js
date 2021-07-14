bb5eea58b6f0cc3f633313447e1fda3a20c87643
farouk afolabi <afolabifarouk99@gmail.com>
2021-07-14 13:19:39 +0300

    submit Events && POST request

--- src/Create.js
+++ src/Create.js
@@ -1,46 +1,55 @@
-import { useState } from "react"
+import { useState } from "react";
+
 const Create = () => {
-    const [title, setTitle] = useState ('');
-    const [body, setBody] = useState ('');
-    const [author, setAuthor] = useState ('yoshi');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
 
-    return ( 
-        <div className="create">
-            <h2>Add a new blog </h2>
-            <form>
-                <label>
-                    <input
-                    type="text"
-                    required
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    />
-                </label>
-                <label> Blog body:</label>
-                <textarea 
-                required
-                value={body}
-                onChange={(e) => setBody(e.target.value)}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
 
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+    })
+  }
 
-                ></textarea>
-                    <label>
-                    Blog author:
-                </label>
-                <select
-                value={author}
-                onChange={(e) => setAuthor(e.target.value)}
-                >
-                    <option value="mario">mario</option>
-                    <option value="yoshi">yoshi</option>
-                </select>
-                <button> Add Blog </button>
-                <p> {title} </p>
-                <p> {body} </p>
-                <p>{author} </p>
-            </form>
-        </div>
-     );
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        { !isPending && <button>Add Blog</button> }
+        { isPending &&  <button disabled >Adding  blog...</button> }
+      </form>
+    </div>
+  );
 }
  
 export default Create;
